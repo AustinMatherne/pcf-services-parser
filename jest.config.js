6ceba8ce559f91247367e4d5f27b3d0e5bca4835
Austin M. Matherne <AustinMatherne@ProtonMail.ch>
2018-11-25 23:21:42 -0600

    add typescript paths support

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const tsPaths = require("./tsconfig").compilerOptions.paths || {};
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   collectCoverage: true,
-  restoreMocks: true
+  restoreMocks: true,
+  moduleNameMapper: pathsToModuleNameMapper(tsPaths, { prefix: "<rootDir>/" })
 };
