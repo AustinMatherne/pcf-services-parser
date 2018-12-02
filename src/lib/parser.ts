72807bf39deb30478f620fb80e134df74e1d51a5
Austin M. Matherne <AustinMatherne@ProtonMail.ch>
2018-12-02 17:47:09 -0600

    test(codecoverage): test code coverage tools with garbage code

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -3,6 +3,91 @@ import { isPlainObject, pipe, snakeCase, toUpper } from "lodash/fp";
 
 export type Credentials = Record<string, any>;
 
+export const functionWithoutCoverage = (
+  flag1: boolean,
+  flag2: boolean,
+  flag3: boolean
+): boolean => {
+  if (flag1) {
+    return flag1;
+  }
+
+  if (flag2) {
+    return flag2;
+  }
+
+  if (flag3) {
+    return flag3;
+  }
+
+  const flags = [flag1, flag2, flag3];
+
+  return flags.some(val => {
+    if (typeCheck(val, "boolean")) {
+      return true;
+    }
+
+    if (typeCheck(val, "function")) {
+      return false;
+    }
+
+    if (typeCheck(val, "number")) {
+      return false;
+    }
+
+    if (typeCheck(val, "object")) {
+      return false;
+    }
+
+    if (typeCheck(val, "string")) {
+      return false;
+    }
+
+    if (typeCheck(val, "symbol")) {
+      return false;
+    }
+
+    if (typeCheck(val, "undefined")) {
+      return false;
+    }
+
+    return false;
+  });
+};
+
+const typeCheck = (val: any, typeString: string) =>
+  typeCheckWrapper(val, typeString);
+
+const typeCheckWrapper = (val: any, typeString: string) =>
+  typeCheckWrapper2(val, typeString);
+
+const typeCheckWrapper2 = (val: any, typeString: string) =>
+  typeCheckWrapper3(val, typeString);
+
+const typeCheckWrapper3 = (val: any, typeString: string) =>
+  typeCheckWrapper4(val, typeString);
+
+const typeCheckWrapper4 = (val: any, typeString: string) =>
+  typeCheckWrapper5(val, typeString);
+
+const typeCheckWrapper5 = (val: any, typeString: string) =>
+  typeCheckWrapper6(val, typeString);
+
+const typeCheckWrapper6 = (val: any, typeString: string) =>
+  typeCheckWrapper7(val, typeString);
+
+const typeCheckWrapper7 = (val: any, typeString: string) =>
+  typeCheckWrapper8(val, typeString);
+
+const typeCheckWrapper8 = (val: any, typeString: string) =>
+  typeCheckWrapper9(val, typeString);
+
+const typeCheckWrapper9 = (val: any, typeString: string) =>
+  typeCheckImpl(val, typeString);
+
+const typeCheckImpl = (val: any, typeString: string) =>
+  typeof val === typeString;
+
 export const getPcfServiceCredentials = (pcfEnv: AppEnv = getAppEnv()) =>
   pipe(
     getServiceNames,
